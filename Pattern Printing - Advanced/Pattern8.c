d84b02ba5ff9a4b144cf4fea215c41ea479bf320
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-24 22:58:51 +0530

    .

--- Pattern Printing - Advanced/Pattern8.c
+++ Pattern Printing - Advanced/Pattern8.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 int main() {
 
-  // int n;
-  // printf("Enter value of n: ");
-  // scanf("%d", &n);
+  int n;
+  printf("Enter value of n: ");
+  scanf("%d", &n);
 
-  for (int row = 1; row <= 5; row++) {
+  for (int row = 1; row <= n; row++) {
+    for (int sp = row; sp < n; sp++) {
+      printf(" ");
     }
+    for (int st = 1; st <= row; st++) {
+      printf("* ");
+    }
+    printf("\n");
+  }
+  for (int row = n; row >= 1; row--) {
+    for (int sp = row; sp < n; sp++) {
+      printf(" ");
+    }
+    for (int st = 1; st <= row; st++) {
+      printf("* ");
+    }
+    printf("\n");
+  }
 
   return 0;
 }
+
+// OUTPUT-
+//    * 
+//   * * 
+//  * * * 
+// * * * *
+// * * * * 
+//  * * *
+//   * *
+//    *
