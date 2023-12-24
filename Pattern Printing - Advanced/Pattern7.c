797e6aa0d2e91f953f86cab81bab1919a6f40209
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-24 22:47:53 +0530

    Pattern7: Add diamond shape pattern printing

--- Pattern Printing - Advanced/Pattern7.c
+++ Pattern Printing - Advanced/Pattern7.c
@@ -5,5 +5,39 @@ int main() {
   printf("Enter a number: ");
   scanf("%d", &n);
 
+  for (int row = 1; row <= n; row++) {
+    for (int col = 1; col <= row; col++) {
+      printf("* ");
+    }
+    for (int sp = 1; sp <= (2 * n) - (2 * row); sp++) {
+      printf("  ");
+    }
+    for (int col = 1; col <= row; col++) {
+      printf("* ");
+    }
+    printf("\n");
+  }
+  for (int row = 1; row <= n - 1; row++) {
+    for (int str = row; str <= n - 1; str++) {
+      printf("* ");
+    }
+    for (int sp = 1; sp <= 2 * row; sp++) {
+      printf("  ");
+    }
+    for (int str = row; str <= n - 1; str++) {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
   return 0;
 }
+
+// OUTPUT-
+// *             *
+// * *         * *
+// * * *     * * *
+// * * * * * * * *
+// * * *     * * *
+// * *         * *
+// *             *
