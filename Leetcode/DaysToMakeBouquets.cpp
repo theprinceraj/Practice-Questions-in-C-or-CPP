9a3079cd49f8d5c107b0a990b7445acb25917d89
Prince Raj <profile.princeraj@gmail.com>
2025-10-04 22:27:55 +0530

    Implement DaysToMakeBouquets solution with binary search and bouquet counting logic

--- Leetcode/DaysToMakeBouquets.cpp
+++ Leetcode/DaysToMakeBouquets.cpp
@@ -1,6 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ * Problem Link: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+*/
+
+int numOfBouquets(int day, int k, vector<int>& bloomDay) {
+    int num = 0, count = 0;
+    for(int i : bloomDay) {
+        if(i <= day) {
+            count++;
+        } else
+            count = 0;
+        if(count == k) {
+            num++;
+            count = 0;
+        }
+    }
+    return num;
+}
+
+void minDays(vector<int>& bloomDay, int m, int k) {
+    if((long)m * k > bloomDay.size())
+        cout << "Answer: " << -1;
+
+    int start = 0, end = 0;
+    for(int i : bloomDay) {
+        end = max(i, end);
+    }
+    int ans = 0;
+    while(start <= end) {
+        int mid = (start + end) / 2;
+        int bouqNum = numOfBouquets(mid, k, bloomDay);
+        if(bouqNum >= m) {
+            ans = mid;
+            end = mid - 1;
+        } else
+            start = mid + 1;
+    }
+    if(ans == 0) {
+        cout << "Answer: " << -1;
+    }
+    cout << "Answer: " << ans;
+}
+
 int main() {
     int n;
     cout << "Enter length of integer array bloomDay: ";
@@ -16,14 +59,12 @@ int main() {
     cout << "Enter number of bouquets to make i.e. m: ";
     cin >> m;
 
-    int n;
-    cout << "Enter number of adjacent flowers that can be taken i.e. n: ";
-    cin >> n;
+    int k;
+    cout << "Enter number of adjacent flowers that can be taken i.e. k: ";
+    cin >> k;
 
-
+    minDays(nums, m, k);
 
     cout << endl;
     return 0;
 }
-
-// UNFINISHED RN
