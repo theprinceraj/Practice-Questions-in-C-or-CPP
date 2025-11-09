7f19490861de1239ea6fdc7920a4919fa4942ec4
Prince Raj <profile.princeraj@gmail.com>
2025-11-09 22:45:36 +0530

    Refactor peakIndexInMountainArrayOptimised function to improve mid calculation and update test case

--- Leetcode/PeakIndexinMountainArray.cpp
+++ Leetcode/PeakIndexinMountainArray.cpp
@@ -18,11 +18,15 @@ int peakIndexInMountainArrayBruteForce(vector<int>& arr) {
 // Time Complexity: O(log (n))
 int peakIndexInMountainArrayOptimised(vector<int>& arr) {
     int start = 0, end = arr.size() - 1;
+    // int start = 1, end = arr.size() - 2;
     while(start <= end) {
-        int mid = start + (end - start) / 2;
+        // We can not use this formula to calculate mid here unless we initialize [start = 1 & end = arr.size()-2] because
+        // below way creates left bias and might give 0 which would lead to arr[-1] being checked and error
+        // int mid = start + (end - start) / 2;
+        int mid = end - (end - start) / 2;
         if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) { //Condn. for peak element
             return mid;
-        } else if(arr[mid] > arr[mid - 1]) { // this way we find out that we are in increasing slope of mountain arry
+        } else if(arr[mid] > arr[mid - 1]) { // this way we find out that we are in the increasing slope of mountain arry
             start = mid + 1;
         } else { // this way we find out that we are in decreasing slope of mountain arry
             end = mid - 1;
@@ -32,7 +36,8 @@ int peakIndexInMountainArrayOptimised(vector<int>& arr) {
 }
 
 int main() {
-    vector<int> arr = {0, 1, 10, 5, 2};
+    vector<int> arr = {3, 5, 3, 2, 0};
+    // vector<int> arr = {0, 2, 1, 0};
     int ans = peakIndexInMountainArrayBruteForce(arr);
     cout << ans << endl;
     return 0;
