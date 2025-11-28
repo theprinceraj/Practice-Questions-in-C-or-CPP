5e7ef6b9b4df480b26008ceed18d551fa2a7b793
Prince Raj <profile.princeraj@gmail.com>
2025-11-28 09:41:51 +0530

    Fix syntax error

--- Leetcode/KokoEatingBananas.cpp
+++ Leetcode/KokoEatingBananas.cpp
@@ -5,50 +5,47 @@ using namespace std;
 
 int minEatingSpeed(vector<int>& piles, int h) {
     int n = piles.size(), ans = -1;
-    int start = 0, end = *max_element(piles.begin(), piles.end());
+    int start = 1, end = *max_element(piles.begin(), piles.end());
     cout << start << " " << end << endl;
 
     while(start <= end) {
-        int n = piles.size(), ans = -1;
-        int start = 1, end = *max_element(piles.begin(), piles.end());
-        cout << start << " " << end << endl;
+        int mid = start + (end - start) / 2;
 
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
+        // int count = 0;
+        long long count = 0;
+        for(int j : piles) {
+            // We calculate the hours using mathematical formula: [pile / speed]
+            // We need to get the ceiling of the above division
+            count += (j + mid - 1) / mid;
 
-            // int count = 0;
-            long long count = 0;
-            for(int j : piles) {
-                // We calculate the hours using mathematical formula: [pile /
-                // speed] We need to get the ceiling of the above division
-                count += (j + mid - 1) / mid;
-
-                // The following approach to calculate hours is correct as well
-                // but it is highly inefficient because it is brute force
-                // calculation. Example: If a pile has 10^9 bananas and your
-                // current eating speed (mid) is 1, this loop runs 1 billion
-                // times for a single pile. This turns your (log N) binary
-                // search into a brute-force O(N) operation while(j > 0) {
-                //     count++;
-                //     j -= mid;
-                // }
-            }
-
-            if(count <= h) {
-                ans = mid;
-                end = mid - 1;
-            } else
-                start = mid + 1;
+            // The following approach to calculate hours is correct as well
+            // but it is highly inefficient because it is brute force
+            // calculation.
+            // Example: If a pile has 10^9 bananas and your
+            // current eating speed (mid) is 1, this loop runs 1 billion
+            // times for a single pile. This turns your (log N) binary
+            // search into a brute-force O(N) operation
+            // while(j > 0) {
+            //     count++;
+            //     j -= mid;
+            // }
         }
 
-        return ans;
+        if(count <= h) {
+            ans = mid;
+            end = mid - 1;
+        } else
+            start = mid + 1;
     }
 
-    int main() {
-        // vector<int> piles = {3, 6, 7, 11};
-        // int h = 8;
+    return ans;
+}
 
-        vector<int> piles = {30, 11, 23, 4, 20};
-        int h = 5;
-        cout << minEatingSpeed(piles, h) << endl;
-    }
+int main() {
+    // vector<int> piles = {3, 6, 7, 11};
+    // int h = 8;
+
+    vector<int> piles = {30, 11, 23, 4, 20};
+    int h = 5;
+    cout << minEatingSpeed(piles, h) << endl;
+}
