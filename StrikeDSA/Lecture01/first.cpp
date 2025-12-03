583190d9221c7c3ddfc47a0b88ad8f833e47b62d
Prince Raj <profile.princeraj@gmail.com>
2025-12-03 11:23:04 +0530

    Add first C++ program and README for Lecture 01
    - Created first.cpp with basic data types and output using std::cout
    - Added a README file with a link to the lecture notes

---
+++ StrikeDSA/Lecture01/first.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// Code likha: std::cout ka meaning, console mein chejo ko print karna hai
+
+int main() {
+
+    // Number
+    // int : 4byte
+    // float: 4byte
+    // char: 1byte
+    // boolean: 1byte
+    int a = 327;
+    float b = 2.41;
+    char c = 'a';
+    bool num = false;
+    long long d = 327298314793712;
+    string name = "Prince Raj";
+
+    // cout<<"a";
+
+    cout << name.length();
+
+    return 0;
+}
